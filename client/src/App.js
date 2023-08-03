86ad9d5d4a7b769872bb852aac1e543d19c7b281
Darsh Shah <102041543+dbs-2012@users.noreply.github.com>
2023-08-03 18:09:07 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
-      Hello World
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
